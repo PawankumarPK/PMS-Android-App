ce453f7a3786af348e7a5e68d3df100c8c4bc71e
PawankumarPK <pk9910765616@gmail.com>
2020-11-02 23:14:36 +0530

    Add recycler with adapter in home fragment

--- app/src/main/java/com/purpleshade/pms/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/purpleshade/pms/fragment/home/HomeFragment.kt
@@ -1,18 +1,22 @@
 package com.purpleshade.pms.fragment.home
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.material.tabs.TabLayout
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.purpleshade.pms.R
 import com.purpleshade.pms.fragment.BaseFragment
+import com.purpleshade.pms.fragment.home.adapter.PasswordsAdapter
 
-class HomeFragment : BaseFragment(){
+class HomeFragment : BaseFragment() {
 
     private lateinit var viewModel: HomeViewModel
+    lateinit var adapter: PasswordsAdapter
+    lateinit var recyclerView: RecyclerView
+    private var passwordList: ArrayList<String> = ArrayList()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.home_fragment, container, false)
@@ -21,5 +25,25 @@ class HomeFragment : BaseFragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        recyclerView = view.findViewById<RecyclerView>(R.id.mRecyclerView)
+        loadAdapter()
+    }
+
+    private fun loadAdapter() {
+        loadList()
+        adapter = PasswordsAdapter(passwordList)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(baseActivity)
+    }
+
+    private fun loadList() {
+        passwordList.add("Google")
+        passwordList.add("Facebook")
+        passwordList.add("WhatsApp")
+        passwordList.add("Gmail")
+        passwordList.add("Instagram")
+        passwordList.add("Linkdin")
+
     }
 }
---
+++ app/src/main/java/com/purpleshade/pms/fragment/home/adapter/PasswordsAdapter.kt
@@ -0,0 +1,37 @@
+package com.purpleshade.pms.fragment.home.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.purpleshade.pms.R
+
+/**
+ * Created by pawan on 02,November,2020
+ */
+class PasswordsAdapter(val passwordList: ArrayList<String>) : RecyclerView.Adapter<PasswordsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.password_viewholder, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return passwordList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.onBind(position)
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title = itemView.findViewById<TextView>(R.id.mTitle)
+
+        fun onBind(pos: Int) {
+            val data = passwordList[pos]
+            title.text = data
+
+        }
+    }
+
+}
