e12b88db121da06ef82373aa1ffad4e9a91f0637
PawankumarPK <pk9910765616@gmail.com>
2021-02-27 14:28:37 +0530

    Update Profile layout done

---
+++ app/src/main/java/com/purpleshade/pms/fragment/profile/editProfile/EditProfileFragment.kt
@@ -0,0 +1,29 @@
+package com.purpleshade.pms.fragment.profile.editProfile
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.purpleshade.pms.R
+import com.purpleshade.pms.fragment.BaseFragment
+
+class EditProfileFragment : BaseFragment() {
+
+    companion object {
+        fun newInstance() = EditProfileFragment()
+    }
+
+    private lateinit var viewModel: EditProfileViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.edit_profile_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(EditProfileViewModel::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/purpleshade/pms/fragment/profile/editProfile/EditProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.purpleshade.pms.fragment.profile.editProfile
+
+import androidx.lifecycle.ViewModel
+
+class EditProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
