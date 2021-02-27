2706f217a4f8b2e836143170f15008b530216956
PawankumarPK <pk9910765616@gmail.com>
2021-02-27 14:23:54 +0530

    Profile layout done

---
+++ app/src/main/java/com/purpleshade/pms/fragment/profile/ProfileFragment.kt
@@ -0,0 +1,29 @@
+package com.purpleshade.pms.fragment.profile
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.purpleshade.pms.R
+import com.purpleshade.pms.fragment.BaseFragment
+
+class ProfileFragment : BaseFragment() {
+
+    companion object {
+        fun newInstance() = ProfileFragment()
+    }
+
+    private lateinit var viewModel: ProfileViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.profile_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/purpleshade/pms/fragment/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.purpleshade.pms.fragment.profile
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
